cc0ce36d8f39b2dc04e4de1ea62bbf4d3331d436
William Lopera <lopera.william@gmail.com>
2019-11-09 22:53:23 -0500

    Inicio inyeccion de dependencias- spring java -2

--- src/main/java/org/wlopera/spring/app/App.java
+++ src/main/java/org/wlopera/spring/app/App.java
@@ -1,9 +1,11 @@
 package org.wlopera.spring.app;
 
+import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Component;
 import org.wlopera.spring.model.Autor;
 import org.wlopera.spring.model.Libro;
@@ -17,18 +19,26 @@ public class App {
 	private Libro libro;
 
 	public void init() {
+		
+		BeanFactory factory = new ClassPathXmlApplicationContext("beans.xml");
+		System.out.println("Uso de XML....................................................");
+		System.out.println("Libro desde xml: " + factory.getBean("libro"));
+		System.out.println("Autor desde xml: " + factory.getBean("autor"));
+	        
 		ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
 
+		System.out.println("\nUso de Anotaciones....................................................");
 		for (String value : context.getBeanDefinitionNames()) {
 			System.out.println(value);
 		}
 		
-		System.out.println("Autor: " + context.getBean(Autor.class).toString());
+		System.out.println("\nAutor: " + context.getBean(Autor.class).toString());
 		System.out.println("Libro: " + context.getBean("libroService").toString());
 
 		System.out.println("Autor: " + context.getBean(App.class).autor);
 		System.out.println("Libro: " + context.getBean(App.class).libro);
 		
+		((ConfigurableApplicationContext)factory).close();
 		((ConfigurableApplicationContext)context).close();
 	}
 
